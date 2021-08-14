0bc668156d8f83dda79de383ad193b915c8544bb
Jlopezr50 <jlopezr50@miumg.edu.gt>
2021-08-14 14:43:16 -0600

    Apartado de comentarios, canal de usuario, boton suscribir y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {EstructuraModule} from "./estructura/estructura.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EstructuraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
